4beee3bdefc71fd51befea5f1a6f94c8fea91b77
Noor Aldeen <anooraldeen9@gmail.com>
2023-12-31 17:36:40 +0200

    add middleware to get user info from token

---
+++ Employee Managment System web API/Middleware/UserMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Employee_Management_System.Model;
+
+namespace Employee_Managment_System_web_API.Middleware
+{
+    public class UserMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public UserMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var user = context.Items["CurrentUser"] as User;
+
+            if (user != null)
+            {
+                context.Items["User"] = user;
+            }
+
+            await _next(context);
+        }
+    }
+}
