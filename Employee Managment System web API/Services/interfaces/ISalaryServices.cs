37f56b8c3c1b5b6dd4d7f154e794ff4eac105eab
Noor Aldeen <anooraldeen9@gmail.com>
2024-01-01 14:39:27 +0200

    add salary services interface

---
+++ Employee Managment System web API/Services/interfaces/ISalaryServices.cs
@@ -0,0 +1,11 @@
+﻿using Employee_Management_System.Model;
+
+namespace Employee_Management_System.Services
+{
+    public interface ISalaryServices
+    {
+        public Task<List<Dictionary<string, string>>> GetEmployeeSalaries();
+        public Task<Salary> GetEmployeeSalaryDetails(string employeeEmail);
+        public Task<List<Employee>> FilterEmployeesBySalary();
+    }
+}
