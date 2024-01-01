b45e652f2dd8cb3beec2dcad869d33021870098d
Noor Aldeen <anooraldeen9@gmail.com>
2024-01-01 16:16:44 +0200

    add attendance services interface

---
+++ Employee Managment System web API/Services/interfaces/IAttendanceServices.cs
@@ -0,0 +1,13 @@
+﻿using Employee_Management_System.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee_Management_System.Services
+{
+    public interface IAttendanceServices
+    {
+        public Task<bool> AddAttendance(Attendance attendance, User user);
+        public Task<(List<Attendance>, bool)> GetAttendancesForDepartment(string departmentName, DateTime? startDate, User user);
+        public Task<List<Attendance>> AttendancesReport(DateTime startDate);
+        public Task<(List<Attendance>, bool)> GetAttendanceForEmployee(User user, string? employeeEmail, DateTime startDate);
+    }
+}
