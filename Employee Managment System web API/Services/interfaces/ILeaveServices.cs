7d2d9f09bec9b62d9eea5d16c58e81b84d7ef6d3
Noor Aldeen <anooraldeen9@gmail.com>
2024-01-02 17:08:05 +0200

    add leave services interface

---
+++ Employee Managment System web API/Services/interfaces/ILeaveServices.cs
@@ -0,0 +1,14 @@
+﻿using Employee_Management_System.Model;
+
+namespace Employee_Management_System.Services
+{
+    public interface ILeaveServices
+    {
+        public Task<bool> AddLeave(Leave leave);
+        public Task<List<Leave>> GetLeaves();
+        public Task<List<Dictionary<string, string>>> LeaveTrend();
+        public Task<(List<Leave>, bool)> GetDepartmentLeaves(User user, string departmentName);
+        public Task<(List<Leave>, bool)> GetPendingLeavesForDepartment(User user, string departmentName);
+        public Task<(List<Leave>, bool)> GetLeavesForEmployee(User user, string? employeeEmail);
+    }
+}
