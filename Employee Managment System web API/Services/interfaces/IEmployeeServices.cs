920fa813ce6e13f372cc137fe6ab49eb1b5d426b
Noor Aldeen <anooraldeen9@gmail.com>
2024-01-02 09:53:24 +0200

    add employee services interface

---
+++ Employee Managment System web API/Services/interfaces/IEmployeeServices.cs
@@ -0,0 +1,12 @@
+﻿using Employee_Management_System.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee_Management_System.Services
+{
+    public interface IEmployeeServices
+    {
+        public Task<bool> AddEmployee(EmployeeCreationModel employeeCreationModel);
+        public Task<List<Employee>> GetEmployees();
+        public Task<(List<Employee>, bool)> GetEmployeesInDepartment(User user, string departmentName);
+    }
+}
