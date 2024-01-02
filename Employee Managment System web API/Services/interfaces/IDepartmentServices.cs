82a83f070c6dbb0df9fc60176bb3411f93f4d74c
Noor Aldeen <anooraldeen9@gmail.com>
2024-01-02 15:17:30 +0200

    add department services interface

---
+++ Employee Managment System web API/Services/interfaces/IDepartmentServices.cs
@@ -0,0 +1,12 @@
+﻿using Employee_Management_System.Model;
+
+namespace Employee_Management_System.Services
+{
+    public interface IDepartmentServices
+    {
+        public Task<bool> AddDepartment(Department department);
+        public Task<List<Department>> GetDepartments();
+        public Task<List<Dictionary<string, string>>> GetDepartmentSatatistics();
+        public Task<bool> UpdateDepartment(Department department);
+    }
+}
